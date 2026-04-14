97816f0301b6e16903bd0c233e6826cda557857a
Diniz-z <devdiniz82@gmail.com>
2026-04-13 22:12:32 -0300

    organizando projeto

---
+++ vh/valorX_ex06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10], n1;
+    for (int i = 0; i < 10; i++) {
+        do {
+            scanf("%d", &vetor[i]);
+        }while (vetor[i] == 0);
+    }
+    printf("Digite um número dos quais vc digitou que vc deseja que seja substituido por zero\n");
+    scanf("%d", &n1);
+    for (int i = 0; i < 10; i++) {
+        if (vetor[i] == n1) {
+            vetor[i] = 0;
+        }
+        printf("%d\n", vetor[i]);
+    }
+    return 0;
+}
